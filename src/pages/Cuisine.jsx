b2fd5efed5017889d0085cbacea3728ad6aca2b4
Catherine Komi <oceanbeaats@gmail.com>
2022-05-30 21:04:25 -0700

    +

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import {motion} from 'framer-motion';
 import {Link, useParams} from 'react-router-dom';
 import React, {useEffect, useState} from 'react'
 function Cuisine() {
